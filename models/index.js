f53cdf177939d911ee9029b07caae87ed79c295f
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2022-07-25 20:45:49 -0400

    Post Model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const User = require('./User');
+const Post = require("./Post");
 
 
 
@@ -7,5 +8,4 @@ const User = require('./User');
 
 
 
-
-module.exports = { User };
+module.exports = { User, Post };
