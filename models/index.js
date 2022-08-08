9fadc77825b6d03ede23868cb84ae990a73fc9be
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2022-08-08 13:13:37 -0400

    Post Done

--- models/index.js
+++ models/index.js
@@ -2,7 +2,14 @@ const User = require('./User');
 const Post = require("./Post");
 
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 
 
