e60379fbebfa4099586184d9b81c54d35a70a31c
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2022-08-11 09:38:20 -0400

    comments done

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 
@@ -8,6 +8,7 @@ const sequelize = require('../../config/connection');
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
+      order: [['created_at', 'DESC']],
       // Query configuration
       attributes: [
         'id',
@@ -16,9 +17,16 @@ router.get('/', (req, res) => {
         'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
-      order: [['created_at', 'DESC']],
-
       include: [
+        //include the Comment model here:
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
         {
           model: User,
           attributes: ['username']
