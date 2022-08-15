435c7904ba175e449218526c89a276a5962541b4
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2022-08-15 14:57:43 -0400

    login logout

--- server.js
+++ server.js
@@ -1,23 +1,76 @@
+// This is the original server.js layout
+
+// const path = require('path');
+// const express = require('express');
+// const routes = require('./controllers');
+// const sequelize = require('./config/connection');
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({});
+
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // turn on routes
+// app.use(routes);
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // turn on connection to db and server
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log('Now listening'));
+// });
+
+
+
+// This is the server js from the bootcamp
+// Some lines of code was rearranged 
+// controllers was changed to app.use
+
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
+// const routes = require('./controllers');
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // turn on routes
-app.use(routes);
+// app.use(routes);
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
